56cc8ae4cd8505dcf912a759f1c7ccb0235304c9
Ilya Fedotov <ifedotov@gmail.com>
2011-05-24 01:00:34 -0400

    Lost line breaks in last commit

--- scripts/test-server.bat
+++ scripts/test-server.bat
@@ -6,5 +6,9 @@ echo Starting JsTestDriver Server (http://code.google.com/p/js-test-driver/)
 echo Please open the following url and capture one or more browsers:
 echo http://localhost:%port%
 
-java -jar "%base_dir%/../test/lib/jstestdriver/JsTestDriver.jar" --port %port% --browserTimeout 20000 --config "%base_dir%/../config/jsTestDriver.conf" --basePath "%base_dir%/.."
+java -jar "%base_dir%/../test/lib/jstestdriver/JsTestDriver.jar" ^
+	--port %port% ^
+	--browserTimeout 20000 ^
+	--config "%base_dir%/../config/jsTestDriver.conf" ^
+	--basePath "%base_dir%/.."
 echo on
