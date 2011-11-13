7b2f4b345c9d851493f6f586c36298341e500721
Michael Moussa <michael.moussa@gmail.com>
2011-11-13 00:06:17 -0500

    Various script fixes
    * Removed initial `cd ..` because it does not work
    
    * Corrected execution instructions
        - "watch" was probably a typo
        - don't need to execute it as an arg to "watchr" - it will run if executed directly
        - need to execute from the root directory or it won't properly watch app/test files (run "watchr -l watchr.rb" vs. "watchr -l ./scripts/watchr.rb" to see list of monitored files)
    
    * Fixed logging
        - ">" will overwrite the log file each time.  need to use ">>" to append
        - removed the "&" from the test execution logging, otherwise output won't be written to log file

--- scripts/watchr.rb
+++ scripts/watchr.rb
@@ -2,18 +2,18 @@
 
 # config file for watchr http://github.com/mynyml/watchr
 # install: gem install watchr
-# run: watch watchr.rb
-# note: make sure that you have jstd server running (server.sh) and a browser captured
+# run: ./scripts/watchr.rb
+# note: make sure that you have jstd server running (server.sh) and a browser
+# captured. this script must be run from the application root directory.
 
 log_file = File.expand_path(File.dirname(__FILE__) + '/../logs/jstd.log')
 
-`cd ..`
 `touch #{log_file}`
 
 puts "String watchr... log file: #{log_file}"
 
 watch( '(app/js|test/unit)' )  do
-  `echo "\n\ntest run started @ \`date\`" > #{log_file}`
-  `scripts/test.sh &> #{log_file}`
+  `echo "\n\ntest run started @ \`date\`" >> #{log_file}`
+  `scripts/test.sh >> #{log_file}`
 end
 
