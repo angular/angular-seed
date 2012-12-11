fa7601c4dde152004f813277d97f812f68ab3170
Jess Hottenstein <jess.hottenstein@gmail.com>
2012-12-10 21:07:41 -0500

    Replacing the angular.module calls with a variable
    * Added controller injection to controller specs
    * Removed module dependencies from myApp module creation

--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,4 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+myApp.value('version', '0.1');
--- test/unit/directivesSpec.js
+++ test/unit/directivesSpec.js
@@ -3,7 +3,7 @@
 /* jasmine specs for directives go here */
 
 describe('directives', function() {
-  beforeEach(module('myApp.directives'));
+  beforeEach(module('myApp'));
 
   describe('app-version', function() {
     it('should print current version', function() {
