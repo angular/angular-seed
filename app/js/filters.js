dfb8aac986ca58e1c20bdd78b1d5870e67b4de6a
lorenzofox3 <laurent34azerty@gmail.com>
2013-04-20 19:00:43 +0200

    add formater

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,9 +2,20 @@
 
 /* Filters */
 
-angular.module('myApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
+angular.module('SmartTable.filters', []).
+    constant('DefaultFilters', ['currency', 'date', 'json', 'lowercase', 'number', 'uppercase']).
+    filter('format', ['$filter', 'DefaultFilters', function (filter, defaultfilters) {
+        return function (value, formatFunction, filterName, filterParameter) {
+
+            var returnFunction;
+
+            if (formatFunction && angular.isFunction(formatFunction)) {
+                returnFunction = formatFunction;
+            } else {
+                returnFunction = defaultfilters.indexOf(filterName) !== -1 ? filter(filterName) : function (value) {
+                    return value
+                };
+            }
+            return returnFunction(value, filterParameter);
+        }
+    }]);
