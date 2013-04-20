a228bada7922e015f347e74dd3f514a03ffa5da9
lorenzofox3 <laurent34azerty@gmail.com>
2013-04-20 19:00:17 +0200

    add sort, move directives to proper file

--- app/js/Table.js
+++ app/js/Table.js
@@ -4,185 +4,146 @@
 
 //TODO be able to register function on remove/add column and rows or use the scope to emit the events
 
-var tableModule = angular.module('SmartTable.Table', ['SmartTable.Column', 'SmartTable.Utilities']);
-tableModule.constant('DefaultTableConfiguration', {
-    selectionMode: 'single',
-    tableTitle:'',
-    dataCollection:'',
-    columns:'',
-    sortFunction:''
-});
-tableModule.controller('TableCtrl', ['$scope', 'Column','$filter','DefaultTableConfiguration', 'SelectUtility', 'ArrayUtility', function (scope, Column,filter, defaultConfig, select, array) {
+angular.module('SmartTable.Table', ['SmartTable.Column', 'SmartTable.Utilities', 'SmartTable.directives', 'SmartTable.filters'])
+    .constant('DefaultTableConfiguration', {
+        selectionMode: 'single',
+        tableTitle: '',
+        dataCollection: '',
+        columns: '',
+        sortFunction: ''
+    })
+    .controller('TableCtrl', ['$scope', 'Column', '$filter', 'DefaultTableConfiguration', 'ArrayUtility', function (scope, Column, filter, defaultConfig, array) {
+
+        //set default value
+        angular.extend(scope, defaultConfig);
 
 
-    //set default value
-    angular.extend(scope, defaultConfig);
+        scope.columns = [];
+        scope.dataCollection = [];
+        scope.displayedCollection = scope.dataCollection;
+        scope.tableTitle = '';
 
-    scope.columns = [];
-    scope.dataCollection = [];
-    scope.displayedCollection = scope.dataCollection;
-    scope.tableTitle = '';
-
-
-    /*////////////
-     Column API
-     ///////////*/
+        var sortFunction;
 
 
-    /**
-     * insert a new column in scope.collection at index or push at the end if no index
-     * @param columnConfig column configuration used to instantiate the new Column
-     * @param index where to insert the column (at the end if not specified)
-     */
-    this.insertColumn = function (columnConfig, index) {
-        var column = new Column(columnConfig);
-        array.insertAt(scope.columns, index, column);
-    };
+        /**
+         * 'select'/'unselect' a entry in an array by setting isSelected on the datarow Model (TODO check if it would be better not to 'pollute' the dataModel itself
+         * and use a wrapper/decorator for all the stuff related to the table features). Select one/multiple rows depending on the selectionMode
+         * @param array the data array where we can select/unselect values
+         * @param selectionMode 'single', 'multiple' or equivalent to 'none'
+         * @param index of the item to select in the array
+         * @param select true if select, false if unselect
+         */
+        function selectDataRow(array, selectionMode, index, select) {
 
-    /**
-     * remove the column at columnIndex from scope.columns
-     * @param columnIndex index of the column to be removed
-     */
-    this.removeColumn = function (columnIndex) {
-        array.removeAt(scope.columns, columnIndex);
-    };
+            if ((!angular.isArray(array)) || (selectionMode !== 'multiple' && selectionMode !== 'single')) {
+                return;
+            }
 
-    /**
-     * move column located at oldIndex to the newIndex in scope.columns
-     * @param oldIndex index of the column before it is moved
-     * @param newIndex index of the column after the column is moved
-     */
-    this.moveColumn = function (oldIndex, newIndex) {
-        array.moveAt(scope.columns,oldIndex,newIndex);
-    };
+            var dataRow;
 
-    /**
-     * set column as the column used to sort the data (if it is already the case, it will change the reverse value)
-     * @param column
-     */
-    this.sortBy=function (column){
-        var index=scope.columns.indexOf(column);
-        if(index!==-1){
-            if(column.isSortable===true){
-                column.sortPredicate=column.sortPredicate || column.map;
-                column.reverse=column.reverse!==true;
-                //if custom sorting function
-                if(scope.sortFunction){
-                    scope.displayedCollection=scope.sortFunction(scope.displayedCollection,column.sortPredicate,column.reverse);
-                }else{
-                    scope.displayedCollection=filter('orderBy')(scope.displayedCollection,column.sortPredicate,column.reverse);
+            if (index >= 0 && index < array.length) {
+                dataRow = array[index];
+                if (selectionMode === 'single') {
+                    //unselect all the others
+                    for (var i = 0, l = array.length; i < l; i++) {
+                        array[i].isSelected = false;
+                    }
+                    dataRow.isSelected = select;
+                } else if (selectionMode === 'multiple') {
+                    dataRow.isSelected = select;
                 }
             }
         }
-    };
-
-    /*///////////
-     ROW API
-     *///////////
-
-    /**
-     * select or unselect the item of the displayedCollection with the selection mode set in the scope
-     * @param dataRow
-     */
-    this.toggleSelection = function (dataRow) {
-        var index=scope.displayedCollection.indexOf(dataRow);
-        if (index!==-1) {
-            select(scope.displayedCollection, scope.selectionMode, index, dataRow.isSelected !== true);
-        }
-    };
-
-    /**
-     * remove the item at index rowIndex from the displayed collection
-     * @param rowIndex
-     * @returns {*} item just removed or undefined
-     */
-    this.removeDataRow = function (rowIndex) {
-        array.removeAt(scope.displayedCollection, rowIndex);
-    };
-
-    /**
-     * move an item from oldIndex to newIndex in displayedCollection
-     * @param oldIndex
-     * @param newIndex
-     */
-    this.moveDataRow = function (oldIndex, newIndex) {
-        array.moveAt(scope.displayedCollection,oldIndex,newIndex);
-    };
-}]);
-
-tableModule.directive('smartTable', ['$log', function (log) {
-    return {
-        restrict: 'E',
-        scope: {
-//            tableTitle: '@',
-//            tableColumns: '=',
-//            tableRows: '='
-        },
-        replace: 'true',
-        templateUrl: 'partials/smartTable.html',
-        controller: 'TableCtrl',
-        link: function (scope, element, attr, ctrl) {
-
-            //TODO for the moment we assign the table ctrl scope here
-            scope.displayedCollection = scope.dataCollection = [
-                {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21')},
-                {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25')},
-                {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27')}
-            ];
 
 
-            scope.columns = [];
+        /*////////////
+         Column API
+         ///////////*/
 
-            ctrl.insertColumn({label: 'FirsName', map: 'firstName',sortPredicate: function (item) {
-                return item.firstName[item.firstName.length-1];
-            }});
-            ctrl.insertColumn({label: 'LastName', map: 'lastName'});
-            ctrl.insertColumn({label: 'birth date', map: 'birthDate'});
 
-            scope.tableTitle = 'super table';
-            //   scope.selectionMode='multiple';
-        }
-    };
-}]);
+        /**
+         * insert a new column in scope.collection at index or push at the end if no index
+         * @param columnConfig column configuration used to instantiate the new Column
+         * @param index where to insert the column (at the end if not specified)
+         */
+        this.insertColumn = function (columnConfig, index) {
+            var column = new Column(columnConfig);
+            array.insertAt(scope.columns, index, column);
+        };
 
-tableModule.directive('smartTableDataRow', function () {
+        /**
+         * remove the column at columnIndex from scope.columns
+         * @param columnIndex index of the column to be removed
+         */
+        this.removeColumn = function (columnIndex) {
+            array.removeAt(scope.columns, columnIndex);
+        };
 
-    return {
-        require: '^smartTable',
-        restrict: 'C',
-        link: function (scope, element, attr, ctrl) {
+        /**
+         * move column located at oldIndex to the newIndex in scope.columns
+         * @param oldIndex index of the column before it is moved
+         * @param newIndex index of the column after the column is moved
+         */
+        this.moveColumn = function (oldIndex, newIndex) {
+            array.moveAt(scope.columns, oldIndex, newIndex);
+        };
 
-            element.bind('click', function () {
-               scope.$apply(function () {
-                   ctrl.toggleSelection(scope.dataRow);
-               })
-            });
-
-            scope.$watch('dataRow.isSelected', function (value) {
-                if (value) {
-                    element.addClass('selected');
-                } else {
-                    element.removeClass('selected');
+        /**
+         * set column as the column used to sort the data (if it is already the case, it will change the reverse value)
+         * @param column
+         */
+        this.sortBy = function (column) {
+            var index = scope.columns.indexOf(column);
+            if (index !== -1) {
+                if (column.isSortable === true) {
+                    column.sortPredicate = column.sortPredicate || column.map;
+                    column.reverse = column.reverse !== true;
+                    //if custom sorting function
+                    if (sortFunction) {
+                        scope.displayedCollection = sortFunction(scope.displayedCollection, column.sortPredicate, column.reverse);
+                    } else {
+                        scope.displayedCollection = filter('orderBy')(scope.displayedCollection, column.sortPredicate, column.reverse);
+                    }
                 }
-            });
-        }
-    }
-});
+            }
+        };
 
-tableModule.directive('smartTableHeaderCell', function () {
-   return {
-       restrict:'C',
-       require:'^smartTable',
-       link: function (scope, element, attr, ctrl) {
-           element.bind('click', function () {
-               scope.$apply(function () {
-                    ctrl.sortBy(scope.column);
-               });
-           });
+        /*///////////
+         ROW API
+         *///////////
 
-       }
-   }
-});
+        /**
+         * select or unselect the item of the displayedCollection with the selection mode set in the scope
+         * @param dataRow
+         */
+        this.toggleSelection = function (dataRow) {
+            var index = scope.displayedCollection.indexOf(dataRow);
+            if (index !== -1) {
+                selectDataRow(scope.displayedCollection, scope.selectionMode, index, dataRow.isSelected !== true);
+            }
+        };
+
+        /**
+         * remove the item at index rowIndex from the displayed collection
+         * @param rowIndex
+         * @returns {*} item just removed or undefined
+         */
+        this.removeDataRow = function (rowIndex) {
+            array.removeAt(scope.displayedCollection, rowIndex);
+        };
+
+        /**
+         * move an item from oldIndex to newIndex in displayedCollection
+         * @param oldIndex
+         * @param newIndex
+         */
+        this.moveDataRow = function (oldIndex, newIndex) {
+            array.moveAt(scope.displayedCollection, oldIndex, newIndex);
+        };
+    }]);
+
+
 
 
 
