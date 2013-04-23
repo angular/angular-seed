81b462447b564d304b736a93258bc299785664b9
lorenzofox3 <laurent34azerty@gmail.com>
2013-04-23 19:51:22 +0200

    simplify code

--- app/js/filters.js
+++ app/js/filters.js
@@ -5,14 +5,14 @@
 angular.module('SmartTable.filters', []).
     constant('DefaultFilters', ['currency', 'date', 'json', 'lowercase', 'number', 'uppercase']).
     filter('format', ['$filter', 'DefaultFilters', function (filter, defaultfilters) {
-        return function (value, formatFunction, filterName, filterParameter) {
+        return function (value, formatFunction, filterParameter) {
 
             var returnFunction;
 
             if (formatFunction && angular.isFunction(formatFunction)) {
                 returnFunction = formatFunction;
             } else {
-                returnFunction = defaultfilters.indexOf(filterName) !== -1 ? filter(filterName) : function (value) {
+                returnFunction = defaultfilters.indexOf(formatFunction) !== -1 ? filter(formatFunction) : function (value) {
                     return value
                 };
             }
