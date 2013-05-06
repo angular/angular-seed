009dd14f96ba6af4d356033bd6ce1815cfa33126
Chris Moultrie <cmoultrie@endgames.us>
2013-05-06 14:52:03 -0400

    test(controllers): use $controller to get hold of a controller for testing
    Closes #75

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -6,11 +6,15 @@ describe('controllers', function(){
   beforeEach(module('myApp.controllers'));
 
 
-  it('should ....', inject(function() {
+  it('should ....', inject(function($controller) {
     //spec body
+    var myCtrl1 = $controller('MyCtrl1');
+    expect(myCtrl1).toBeDefined();
   }));
 
-  it('should ....', inject(function() {
+  it('should ....', inject(function($controller) {
     //spec body
+    var myCtrl2 = $controller('MyCtrl2');
+    expect(myCtrl2).toBeDefined();
   }));
 });
