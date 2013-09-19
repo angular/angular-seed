1f71b8dff7f638e9602ae0060ad0cb17341f8af2
Mark Fayngersh <mark@crowdtwist.com>
2013-09-18 23:28:39 -0400

    Build assets

--- Smart-Table.debug.js
+++ Smart-Table.debug.js
@@ -109,11 +109,9 @@
                         }
                     }, true);
 
-                    //if item are added or removed into the data model from outside the grid
-                    scope.$watch('dataCollection.length', function (oldValue, newValue) {
-                        if (oldValue !== newValue) {
-                            ctrl.sortBy();//it will trigger the refresh... some hack ?
-                        }
+                    //if the data model changes from outside the grid
+                    scope.$watchCollection('dataCollection', function (newValue, oldValue) {
+                        ctrl.sortBy();//it will trigger the refresh... some hack ?
                     });
 
                 }
