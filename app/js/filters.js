ac40b5e609d1ea518f9c998485284f42de762a63
Anders Sundman <anders@4zm.org>
2013-10-12 14:59:58 +0200

    style(filters): add missing semicolon

--- app/js/filters.js
+++ app/js/filters.js
@@ -6,5 +6,5 @@ angular.module('myApp.filters', []).
   filter('interpolate', ['version', function(version) {
     return function(text) {
       return String(text).replace(/\%VERSION\%/mg, version);
-    }
+    };
   }]);
