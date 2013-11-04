8711f6eeab122dc0be873a1a4ccb6e2c0eab55fd
Jeff Cross <middlefloor@gmail.com>
2013-11-04 09:13:22 -0800

    feat(e2e): Used local karma to run e2e tests

--- scripts/e2e-test.sh
+++ scripts/e2e-test.sh
@@ -4,6 +4,7 @@ BASE_DIR=`dirname $0`
 
 echo ""
 echo "Starting Karma Server (http://karma-runner.github.io)"
+echo $BASE_DIR
 echo "-------------------------------------------------------------------"
 
-karma start $BASE_DIR/../config/karma-e2e.conf.js $*
+$BASE_DIR/../node_modules/karma/bin/karma start $BASE_DIR/../config/karma-e2e.conf.js $*
