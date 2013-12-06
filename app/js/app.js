2e0d2b5bad128c27fe187cc37f9ecd2393bfb2e6
Jim Balhoff <balhoff@nescent.org>
2013-12-06 15:50:26 -0600

    Decided to stay with hash URLs for the time being.

--- app/js/app.js
+++ app/js/app.js
@@ -9,9 +9,8 @@ angular.module('myApp', [
   'myApp.directives',
   'myApp.controllers'
 ]).
-config(function ($locationProvider, $routeProvider) {
-  $locationProvider.html5Mode(true);  
-  $routeProvider.when('/view1', {templateUrl: '/partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: '/partials/partial2.html', controller: 'MyCtrl2'});
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
-});
+}]);
