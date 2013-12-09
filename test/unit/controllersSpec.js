5b5a41fb5bdd6f665f1b2d60aef874c295a798df
mebusw <mebusw@163.com>
2013-12-09 14:25:08 +0800

    add sample test for ng-controller

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,18 +3,23 @@
 /* jasmine specs for controllers go here */
 
 describe('controllers', function(){
+
   beforeEach(module('myApp.controllers'));
 
+  it('should increase todo list', inject(function($rootScope, $controller) {
+    var scope = $rootScope.$new();
+    var ctrl = $controller('MyCtrl2', {$scope: scope});
 
-  it('should ....', inject(function() {
-    //spec body
-    expect(true).toBeTruthy();
-    expect(1).toBe(1);
-    expect(1).toEqual(1);
-    // var c1 = controller('MyCtrl1');
+    expect(scope.todos.length).toBe(2);
+
+    scope.addTodo();
+    expect(scope.todos.length).toBe(3);
+
   }));
 
   it('should ....', inject(function() {
-    //spec body
+    expect(true).toBeTruthy();
+    expect(1).toBe(1);
+    expect(1).toEqual(1);
   }));
 });
