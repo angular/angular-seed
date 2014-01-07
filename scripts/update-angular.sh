0ff2b4b1b7e648dfff469dcc857fdf3874ba26e3
Tobias Bosch <tbosch1009@gmail.com>
2014-01-07 13:29:32 -0800

    chore(build): refactor update angular and run tests

--- scripts/update-angular.sh
+++ scripts/update-angular.sh
@@ -5,6 +5,12 @@ if [[ ! -e "$NG_BUILD_DIR/angular.js" ]]; then
   exit 1
 fi
 
+SCRIPT_DIR=$(dirname $0)
+ROOT_DIR=$SCRIPT_DIR/../
+VERSION=$(cat $NG_BUILD_DIR/version.txt)
+
+cd $ROOT_DIR
+
 rm -fr app/lib/angular
 mkdir app/lib/angular
 cp -r $NG_BUILD_DIR/* app/lib/angular
@@ -13,3 +19,13 @@ rm app/lib/angular/*.zip
 mv app/lib/angular/angular-mocks.js test/lib/angular
 mv app/lib/angular/angular-scenario.js test/lib/angular
 cp app/lib/angular/version.txt test/lib/angular
+
+# Update the inlined angular-loader in app/index-async.html
+sed '/@@NG_LOADER@@/{
+    s/@@NG_LOADER@@//g
+    r app/lib/angular/angular-loader.min.js
+}' app/index-async.html.template > app/index-async.html
+
+git add $ROOT_DIR/app
+git add $ROOT_DIR/test
+git commit -m "update(angular): bump to $VERSION"
