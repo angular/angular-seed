48a4dfecf3bb991b532079c719997c685fe3002e
Matt Broadstone <mbroadstone@devonit.com>
2014-03-13 14:08:50 -0400

    refactor(app): refactor app layout to conform to best practices
    The current layout of angular-seed conforms to Cliff Meyers' "piles on the floor"
    layout. This commit attempts to refactor the layout to the emerging best practices
    for angular app layout, grouping files structurally vs by functionality.

--- app/common/version/version-service.js
+++ app/common/version/version-service.js
@@ -2,8 +2,7 @@
 
 /* Services */
 
-
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
+angular.module('myApp.version.service', []).
   value('version', '0.1');
