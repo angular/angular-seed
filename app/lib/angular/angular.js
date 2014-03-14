f7a6fe1dcaa134e9e2a256c27037eb4a62175227
jenkins <ci-bot@angularjs.org>
2014-03-14 16:46:54 -0700

    update(angular): bump to 1.3.0-beta.2

--- app/lib/angular/angular.js
+++ app/lib/angular/angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-beta.2+
+ * @license AngularJS v1.3.0-beta.2
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -68,7 +68,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-beta.2+/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-beta.2/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -1878,7 +1878,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-beta.2+',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-beta.2',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: undefined,
