79f04b19f79a8f1f6f9802e9cd2db6181c95bdcc
jenkins <ci-bot@angularjs.org>
2014-03-14 16:12:39 -0700

    update(angular): bump to 1.3.0-beta.2+

--- app/lib/angular/angular-scenario.js
+++ app/lib/angular/angular-scenario.js
@@ -9790,7 +9790,7 @@ if ( typeof module === "object" && module && typeof module.exports === "object" 
 })( window );
 
 /**
- * @license AngularJS v1.3.0-build.51+sha.e888dde
+ * @license AngularJS v1.3.0-beta.2+
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -9860,7 +9860,7 @@ function minErr(module) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-build.51+sha.e888dde/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-beta.2+/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -11670,7 +11670,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-build.51+sha.e888dde',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-beta.2+',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: undefined,
