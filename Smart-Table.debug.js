5b733b2de680b38f221425e7554c53eded76e665
lorenzofox3 <laurent34azerty@gmail.com>
2014-04-11 16:42:55 +0700

    fix(bug when value not set in config)

--- Smart-Table.debug.js
+++ Smart-Table.debug.js
@@ -654,7 +654,7 @@ angular.module("partials/globalSearchCell.html", []).run(["$templateCache", func
 angular.module("partials/pagination.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("partials/pagination.html",
     "<div class=\"pagination\">\n" +
-    "    <ul>\n" +
+    "    <ul class=\"pagination\">\n" +
     "        <li ng-repeat=\"page in pages\" ng-class=\"{active: page.active, disabled: page.disabled}\"><a\n" +
     "                ng-click=\"selectPage(page.number)\">{{page.text}}</a></li>\n" +
     "    </ul>\n" +
