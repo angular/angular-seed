fbfde7c2cfb6fdbc661e22ad96b47d568b80eeea
Peter Bacon Darwin <pete@bacondarwin.com>
2014-05-03 19:41:16 +0100

    feat(myApp.controllers): add $scope param to clarify annotation
    Closes #154

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,9 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', [function() {
+  .controller('MyCtrl1', ['$scope', function($scope) {
 
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('MyCtrl2', ['$scope', function($scope) {
 
   }]);
