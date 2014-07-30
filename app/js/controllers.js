6fa1a057d07a17344d28fea035a69fc7d2b4541b
ivanmetla <metlaivan@gmail.com>
2014-07-30 21:39:07 +0300

    rebace fields

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -30,6 +30,22 @@ angular.module('myApp.controllers', [])
             iFrame.contentWindow.save();
         }
 
+        $scope.send = function() {
+            console.log("send");
+
+            var exec = require('child_process').exec, child;
+
+            child = exec('cat *.js bad_file | wc -l',
+                function (error, stdout, stderr) {
+                    console.log('stdout: ' + stdout);
+                    console.log('stderr: ' + stderr);
+                    if (error !== null) {
+                        console.log('exec error: ' + error);
+                    }
+                });
+            child();
+        }
+
         $scope.getHeight = function() {
             return window.innerHeight - 100;
         }
