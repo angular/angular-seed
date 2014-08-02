6919ca89098614e9a688c2f83e0ef4ee8ac1d26d
Addison <Addisonedwardlee@gmail.com>
2014-08-01 18:15:45 -0700

    chore(architecture): fix index file and view modules to match file structure

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -3,7 +3,7 @@
 angular.module('view1', [])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {
-      templateUrl: 'components/view1/view1.html',
+      templateUrl: 'view1/view1.html',
       controller: 'MyCtrl1'
     });
   }])
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -3,7 +3,7 @@
 angular.module('view2', [])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view2', {
-      templateUrl: 'components/view2/view2.html',
+      templateUrl: 'view2/view2.html',
       controller: 'MyCtrl2'
     });
   }])
