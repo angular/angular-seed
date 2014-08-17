81e27a2f3aba3c98ae935e7e6f072ceb487ee6a2
lorenzofox3 <laurent34azerty@gmail.com>
2014-08-17 19:54:34 +0700

    wipe master

--- scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BASE_DIR=`dirname $0`
-
-echo ""
-echo "Starting Karma Server"
-echo "-------------------------------------------------------------------"
-
-karma start $BASE_DIR/../config/karma.conf.js $*
