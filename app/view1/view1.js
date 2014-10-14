3fadc6f82a25fe9ea23a71814afb401f5582532a
Clay Anderson <clay_anderson@trimble.com>
2014-10-14 08:34:01 -0600

    Added model (scope) and tests

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,6 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
+.controller('View1Ctrl', ['$scope', function($scope) {
+    $scope.message = 'angular fun';
 }]);
--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -5,12 +5,27 @@ describe('myApp.view1 module', function() {
   beforeEach(module('myApp.view1'));
 
   describe('view1 controller', function(){
-
-    it('should ....', inject(function($controller) {
-      //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
+    var scope;
+    var controller;
+    
+    beforeEach(inject(function ($rootScope, $controller) {
+        scope = $rootScope.$new();
+        controller = $controller('View1Ctrl', {
+            '$scope': scope
+        });
     }));
+    
+    it('should be created', function() {
+      expect(controller).toBeDefined();
+    });
+
+    it('should populate message', function() {
+      expect(scope.message).toBeDefined();
+    });
+
+    it('should populate message exactly', function() {
+      expect(scope.message).toEqual('angular fun');
+    });
 
   });
 });
