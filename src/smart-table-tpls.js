0adaecf64d9c890ec15ddea1fef7d61930274ee4
lorenzofox3 <laurent34azerty@gmail.com>
2014-10-24 08:52:21 +0700

    do not create extra module for pagination template

--- src/smart-table-tpls.js
+++
@@ -1,8 +0,0 @@
-angular.module('smart-table-tpls', ['template/smart-table/pagination.html']);
-
-angular.module('template/smart-table/pagination.html', []).run(['$templateCache', function($templateCache) {
-  $templateCache.put('template/smart-table/pagination.html',
-      '<div class="pagination" ng-if="pages.length >= 2"><ul class="pagination">' +
-      '<li ng-repeat="page in pages" ng-class="{active: page==currentPage}"><a ng-click="selectPage(page)">{{page}}</a></li>' +
-      '</ul></div>');
-}]);
--- src/smart-table.module.js
+++ src/smart-table.module.js
@@ -1,4 +1,9 @@
 (function (ng) {
-  'use strict';
-  ng.module('smart-table',['smart-table-tpls']);
+    'use strict';
+    ng.module('smart-table', []).run(['$templateCache', function ($templateCache) {
+        $templateCache.put('template/smart-table/pagination.html',
+                '<div class="pagination" ng-if="pages.length >= 2"><ul class="pagination">' +
+                '<li ng-repeat="page in pages" ng-class="{active: page==currentPage}"><a ng-click="selectPage(page)">{{page}}</a></li>' +
+                '</ul></div>');
+    }]);
 })(angular);
