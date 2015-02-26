cb23fa1b5e96e4a1fe5fbe6e0ef4a94547c34524
lorenzofox3 <laurent34azerty@gmail.com>
2015-02-26 09:48:42 +0700

    remove double pagination class

--- src/smart-table.module.js
+++ src/smart-table.module.js
@@ -1,7 +1,7 @@
 ng.module('smart-table', []).run(['$templateCache', function ($templateCache) {
     $templateCache.put('template/smart-table/pagination.html',
-        '<div class="pagination" ng-if="pages.length >= 2"><ul class="pagination">' +
+        '<nav ng-if="pages.length >= 2"><ul class="pagination">' +
         '<li ng-repeat="page in pages" ng-class="{active: page==currentPage}"><a ng-click="selectPage(page)">{{page}}</a></li>' +
-        '</ul></div>');
+        '</ul></nav>');
 }]);
 
