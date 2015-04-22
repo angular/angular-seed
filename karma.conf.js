6adc6ff5bd37dbdfbb74966c7a14a248465b739a
Alex Robertson <alex@someoddpilot.com>
2015-04-22 10:08:05 -0500

    Fixing indentation for karma.conf.js plugins

--- karma.conf.js
+++ karma.conf.js
@@ -18,11 +18,11 @@ module.exports = function(config){
     browsers : ['Chrome'],
 
     plugins : [
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-jasmine',
-            'karma-junit-reporter'
-            ],
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-jasmine',
+      'karma-junit-reporter'
+    ],
 
     junitReporter : {
       outputFile: 'test_out/unit.xml',
