37e7944ea63bbae1f001ea42d2a545d5010da5cb
Craig Fisk <shoujigui@gmail.com>
2015-05-24 21:49:45 -0700

    From scratch part1 (just the Angular side; no Firebase, et). This is a second try, because on the first try I did not figure out that the script to include home/home.js must go in the body of index.html and not in the head

--- app/app.js
+++ app/app.js
@@ -1,12 +1,13 @@
 'use strict';
-
-// Declare app level module which depends on views, and components
+ 
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'myApp.home'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({
+    redirectTo: '/home'
+  });
+  // Routes will be here
+  
 }]);
---
+++ app/home/home.js
@@ -0,0 +1,16 @@
+'use strict';
+ 
+angular.module('myApp.home', ['ngRoute'])
+ 
+// Declared route 
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/home', {
+        templateUrl: 'home/home.html',
+        controller: 'HomeCtrl'
+    });
+}])
+ 
+// Home controller
+.controller('HomeCtrl', [function() {
+ 
+}]);
