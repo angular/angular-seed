851df6be38f2f6d04698774d03c24481620a0309
Craig Fisk <shoujigui@gmail.com>
2015-05-29 00:24:23 -0700

    Updates to jshintrc, home/home.js and home.html, and app/index.html

--- app/home/home.js
+++ app/home/home.js
@@ -1,6 +1,6 @@
 'use strict';
  
-angular.module('myApp.home', ['ngRoute'])
+angular.module('myApp.home', ['ngRoute', 'firebase'])
  
 // Declared route 
 .config(['$routeProvider', function($routeProvider) {
@@ -11,6 +11,38 @@ angular.module('myApp.home', ['ngRoute'])
 }])
  
 // Home controller
-.controller('HomeCtrl', [function() {
+.controller('HomeCtrl', ['$scope','$firebaseSimpleLogin',function($scope,$firebaseSimpleLogin) {
  
+  // Added all following for Firebase login:
+  
+  
+  //$scope.SignIn = function($scope) {
+  //  var username = $scope.user.email;
+  //  var password = $scope.user.password;
+     
+    // Auth Logic will be here
+  //};
+  
+  $scope.SignIn = function(event) {
+    event.preventDefault();  // To prevent form refresh
+    var username = $scope.user.email;
+    var password = $scope.user.password;
+     
+    loginObj.$login('password', {
+            email: username,
+            password: password
+        })
+        .then(function(user) {
+            // Success callback
+            console.log('Authentication successful');
+        }, function(error) {
+            // Failure callback
+            console.log('Authentication failure');
+        });
+  };
+  
+  var firebaseObj = new Firebase("https://tuts-scratch.firebaseio.com");
+  
+  var loginObj = $firebaseSimpleLogin(firebaseObj);
+  
 }]);
