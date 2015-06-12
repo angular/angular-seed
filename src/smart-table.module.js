7f8911a7f68abefee4d51c0766cbba069441087a
lorenzofox3 <laurent34azerty@gmail.com>
2015-06-12 10:47:42 +0700

    fix #406

--- src/smart-table.module.js
+++ src/smart-table.module.js
@@ -1,6 +1,6 @@
 ng.module('smart-table', []).run(['$templateCache', function ($templateCache) {
     $templateCache.put('template/smart-table/pagination.html',
-        '<nav ng-if="pages.length >= 2"><ul class="pagination">' +
+        '<nav ng-if="numPages && pages.length >= 2"><ul class="pagination">' +
         '<li ng-repeat="page in pages" ng-class="{active: page==currentPage}"><a ng-click="selectPage(page)">{{page}}</a></li>' +
         '</ul></nav>');
 }]);
