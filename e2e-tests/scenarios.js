a50f4c8a7a9c90942fac38aeb35ea1be183d2a76
Peter Bacon Darwin <pete@bacondarwin.com>
2015-06-15 13:41:06 +0100

    chore(protractor): update to use protractor v2.1.0

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -4,9 +4,9 @@
 
 describe('my app', function() {
 
-  browser.get('index.html');
 
   it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+    browser.get('index.html');
     expect(browser.getLocationAbsUrl()).toMatch("/view1");
   });
 
