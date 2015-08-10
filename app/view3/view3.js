1ad2f96581da9bd2debb3d5a4027fe86601ed85f
Qi Qi <qiqiatosu@gmail.com>
2015-08-09 20:09:54 -0700

    update views to use ui-router

--- app/view3/view3.js
+++ app/view3/view3.js
@@ -1,12 +1,6 @@
 'use strict';
 
-angular.module('myApp.view3', ['ngRoute'])
-
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view3', {
-    templateUrl: 'view3/view3.html'
-  });
-}])
+angular.module('myApp.view3', [])
 
 .controller('View3Ctrl', ['$scope', function($scope) {
 }])
