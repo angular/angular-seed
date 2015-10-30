f48021f8d41c12ca7d2388059ebe049901cfda26
Jesse Clark <jesse.clark@unco.edu>
2015-10-30 10:15:21 -0600

    Move view1 and view2 into views folder.

--- app/views/view1/view1.js
+++ app/views/view1/view1.js
@@ -4,7 +4,7 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
+    templateUrl: 'views/view1/view1.html',
     controller: 'View1Ctrl'
   });
 }])
--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function(config){
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/components/**/*.js',
-      'app/view*/**/*.js'
+      'app/views/**/*.js'
     ],
 
     autoWatch : true,
