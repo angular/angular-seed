b1becdf2d8ac3b254c573c66e24efaa05d3578de
jccultima123 <johncyrillcorsanes@gmail.com>
2015-11-21 15:25:39 +0800

    Cleaner Drivers List

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,25 @@
-angular.module('F1FeederApp', [
+var F1FeederApp = angular.module('F1FeederApp', [
   'F1FeederApp.services',
   'F1FeederApp.controllers',
   'ngRoute'
-]).
-config(['$routeProvider', function($routeProvider) {
+]);
+
+//Routes
+F1FeederApp.config(['$routeProvider', function($routeProvider, $templateCache) {
   $routeProvider.
-	when("/drivers", {templateUrl: "partials/drivers.html", controller: "driversController"}).
-	when("/drivers/:id", {templateUrl: "partials/driver.html", controller: "driverController"}).
-	otherwise({redirectTo: '/drivers'});
+    when("/drivers", {
+        templateUrl: "partials/drivers.html", controller: "driversController"
+    }).
+	when("/drivers/:id", {
+        templateUrl: "partials/driver.html", controller: "driverController"
+    }).
+    otherwise({
+        redirectTo: '/drivers'
+    });
+    // use the HTML5 History API
+    // $locationProvider.html5Mode(true);
 }]);
+
+//F1FeederApp.run(function($templateCache) {
+//    $templateCache.put('/drivers', '');
+//});
