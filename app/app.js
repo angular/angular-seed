06c0b59f04557247bbe9a09791c98dcf3a261762
Evan Paige <ewpaige@gmail.com>
2016-01-01 14:08:41 -0800

    backgroundColor

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
+  'myApp.view3',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
---
+++ app/view3/view3_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.view3 module', function() {
+
+  beforeEach(module('myApp.view3'));
+
+  describe('view3 controller', function() {
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var viewCtrl = $controller('viewCtrl');
+      expect(viewCtrl).toBeDefined();
+    }));
+  
+  });
+});
