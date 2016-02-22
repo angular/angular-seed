bd3ee937da037c32b88d91f857ce89148e1a325f
euniceschoi <euniceschoi93@gmail.com>
2016-02-21 22:12:15 -0800

    add todo list

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,26 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope',function($scope) {
+  $scope.todos = [
+      {text:'Learn AngularJS', done:false},
+      {text: 'Build an app', done:false}
+    ];
+
+    $scope.getTotalTodos = function () {
+      return $scope.todos.length;
+    };
+
+
+    $scope.addTodo = function () {
+      $scope.todos.push({text:$scope.formTodoText, done:false});
+      $scope.formTodoText = '';
+    };
+
+      $scope.clearCompleted = function () {
+          $scope.todos = _.filter($scope.todos, function(todo){
+              return !todo.done;
+          });
+      };
 
 }]);
