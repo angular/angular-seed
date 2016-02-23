2aeb45ba4e2693e04d509ddce00ef7705d72408d
euniceschoi <euniceschoi93@gmail.com>
2016-02-22 23:47:07 -0800

    add reasons why I love Course Hero to view1 controller

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -20,6 +20,15 @@ angular.module('myApp.view1', ['ngRoute'])
       {text:'Get Hired!',done:false}
     ];
 
+  $scope.reasonsWhyCourseHero = [
+      {text:'Course Hero is a leading digital learning platform that serves more than 5 million users and offers more than 7 million course-specific materials to students',icon:"fa fa-camera-retro fa-lg"},
+      {text:"I believe in Course Hero's mission to employ educational technology and help students get the most out of their education",icon:"fa fa-camera-retro fa-lg"},
+      {text:'I love the power of knowledge sharing!',icon:"fa fa-camera-retro fa-lg"},
+      {text:'Working at Course Hero provides the opportunity to help build great products in an energetic and collaborative environment',icon:"fa fa-camera-retro fa-lg"},
+      {text:'The Course Hero Knowledge Drive has donated 249,254 books through Books for Africa and continues to help out students abroad.',icon:"fa fa-camera-retro fa-lg"},
+      {text:'Course Hero has been named as a 2015 top workplace in Bay Area',icon:"fa fa-camera-retro fa-lg"}
+  ];
+
     $scope.addTodo = function () {
       $scope.todos.push({text:$scope.formTodoText, done:false});
       $scope.formTodoText = '';
