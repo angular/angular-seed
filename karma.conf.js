8fd55c34c6d81c542ba27071bd14150092409eb3
Georgios Kalpakas <g.kalpakas@hotmail.com>
2016-04-12 10:53:04 +0300

    chore: update several config files (e.g. for jshint, Travis, Karma, Protractor)
    This brings the config files on par with the tutorial (see angular/angular-phonecat#326).
    It also updates the version of Node used on Travis to `4.4`.
    
    Related to #329.

--- karma.conf.js
+++ karma.conf.js
@@ -1,30 +1,31 @@
-module.exports = function(config){
+//jshint strict: false
+module.exports = function(config) {
   config.set({
 
-    basePath : './',
+    basePath: './app',
 
-    files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/components/**/*.js',
-      'app/view*/**/*.js'
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'components/**/*.js',
+      'view*/**/*.js'
     ],
 
-    autoWatch : true,
+    autoWatch: true,
 
     frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+    browsers: ['Chrome'],
 
-    plugins : [
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-jasmine',
-            'karma-junit-reporter'
-            ],
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-jasmine',
+      'karma-junit-reporter'
+    ],
 
-    junitReporter : {
+    junitReporter: {
       outputFile: 'test_out/unit.xml',
       suite: 'unit'
     }
