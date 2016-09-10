19a5d71103c2044904975a817d8b04757def8967
Rachel Rui Liu <liuruibnu@gmail.com>
2016-09-10 12:41:06 +0800

    feature1-partial

--- app/feature1/feature1.js
+++ app/feature1/feature1.js
@@ -1,14 +1,44 @@
 'use strict';
 
-angular.module('myApp.feature1', ['ngRoute'])
+/*
+ui.bootstrap is needed for a lot of UI related function, like dropdown menu with typeahead
+ngResource is used to call Restful services
+*/
+var feature1App=angular.module('myApp.feature1', ['ngRoute','ui.bootstrap','ngResource']);
 
-.config(['$routeProvider', function($routeProvider) {
+feature1App.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/feature1', {
     templateUrl: 'feature1/feature1.html',
     controller: 'Feature1Ctrl'
   });
-}])
+}]);
 
-.controller('Feature1Ctrl', [function() {
+
+/*
+implement how to invoke rest services with different data types
+json
+json string array
+jsonArray with json objects
+*/
+feature1App.factory("connStrList", function() {
+  var connStrList = ["ConnList1://aaa",
+    "ConnList1://bbb"
+  ];
+  return connStrList;
+});
+
+
+feature1App.controller('Feature1Ctrl', ['$scope','connStrList','$log',function($scope,connStrList,$log) {
+  $log.debug(connStrList);
+  $scope.connStrList=connStrList;
+
+}]);
+
+feature1App.controller('GetListByHttpCallCtrl', ['$scope','$http','$log',function($scope,$http,$log) {
+
+   $http.get('http://localhost:8081/stringArray').success(function(data){
+      $log.debug(data);
+      $scope.connStrList1 =  data;
+   });
 
 }]);
---
+++ app/feature1/readme.md
@@ -0,0 +1,6 @@
+# call restful service exposed by mule.
+(muleside need special config to allow cross domain invoke)
+
+1) dropdown list data from a restful service which return String Array list
+bit of tricky , refer to
+https://mariuszprzydatek.com/2013/12/13/tricky-behavior-of-angularjs-resource-service/
