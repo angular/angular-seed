6b4ce75906c27447f4dc19b55498259b2f76789e
sarasaad3885 <collegeapps3885@yahoo.com>
2016-09-26 23:29:38 -0400

    Create home.js

---
+++ app/home/home.js
@@ -0,0 +1,36 @@
+'use strict';
+ 
+angular.module('myApp.home', ['ngRoute'])
+ 
+// Declared route 
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/home', {
+        templateUrl: 'home/home.html',
+        controller: 'HomeCtrl'
+    });
+}])
+ 
+// Home controller
+.controller('HomeCtrl', ['$scope','$firebaseSimpleLogin',function($scope,$firebaseSimpleLogin) {
+ $scope.SignIn = function(event) {
+    event.preventDefault();  // To prevent form refresh
+    var username = $scope.user.email;
+    var password = $scope.user.password;
+     
+    loginObj.$login('password', {
+            email: username,
+            password: password
+        })
+        .then(function(user) {
+            // Success callback
+            console.log('Authentication successful');
+        }, function(error) {
+            // Failure callback
+            console.log('Authentication failure');
+        });
+}
+ var firebaseObj = new Firebase("https://temputrip.firebaseio.com"); 
+ var loginObj = $firebaseSimpleLogin(firebaseObj);
+}]);
+
+angular.module('myApp.home', ['ngRoute','firebase'])
