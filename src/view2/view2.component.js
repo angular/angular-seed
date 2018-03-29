4c03b5e7fb7ccdcb5f3dcb5412f7cc4ed4a1e266
awhiting93 <andrewwhiting.bba@gmail.com>
2018-03-29 09:57:47 -0400

    Added buttons to switch views

--- src/view2/view2.component.js
+++ src/view2/view2.component.js
@@ -3,12 +3,20 @@ import template from './view2.html';
 export const View2Component = {
     template,
     controller: class View2Component {
-        constructor() {
+        constructor($state) {
             'ngInject';
+
+            this.$state = $state;
+
+            this.title = 'VIEW 2';
         }
 
         $onInit() {
-            this.title = 'VIEW 2';
+            this.message = 'ALL YOUR BASE ARE BELONG TO US';
+        }
+
+        navigateToView1(){
+            this.$state.go('view1');
         }
     }
 };
