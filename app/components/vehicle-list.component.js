9c32fa1b532e1eb4c8257b35d8b6ba7a1d5a9954
Bernice Abreu <berniceabreu@Bernices-MacBook-Pro.local>
2018-08-31 11:24:56 -0400

    feat: filter and orderby

--- app/components/vehicle-list.component.js
+++ app/components/vehicle-list.component.js
@@ -1,18 +1,15 @@
 'use strict';
 
 // Register `phoneList` component, along with its associated controller and template
-angular.
-    module('myApp').
-    component('vehicleList',{
-        // Note: The URL is relative to our `index.html` file
-        templateUrl: '/components/vehicle-list.html',
-        controller: function VehicleController(
-            $scope,
-            $http
-        ) {
-            $scope.vehicles = [];
-            $http.get('./vehicles.json').then(function(result) {
-              $scope.vehicles = result.data;
-            });
-        }
-    })
+angular.module('myApp').component('vehicleList', {
+  // Note: The URL is relative to our `index.html` file
+  templateUrl: '/components/vehicle-list.html',
+
+  controller: function VehicleController($scope, $http) {
+    $scope.vehicles = [];
+    $http.get('./vehicles.json').then(function(result) {
+      $scope.vehicles = result.data;
+    });
+    this.orderProp = 'year';
+  }
+});
