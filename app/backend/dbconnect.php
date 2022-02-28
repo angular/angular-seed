efc16bb83eae6147a6b0103f00aed110e5cf007b
AVS42071 <96913756+AVS42071@users.noreply.github.com>
2022-02-27 22:45:30 -0600

    c v

---
+++ app/backend/dbconnect.php
@@ -0,0 +1,20 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$conn = new mysqli("myServer", "myUser", "myPassword", "Northwind");
+
+$result = $conn->query("SELECT CompanyName, City, Country FROM Customers");
+
+$outp = "";
+while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+  if ($outp != "") {$outp .= ",";}
+  $outp .= '{"Name":"'  . $rs["CompanyName"] . '",';
+  $outp .= '"City":"'   . $rs["City"]        . '",';
+  $outp .= '"Country":"'. $rs["Country"]     . '"}';
+}
+$outp ='{"records":['.$outp.']}';
+$conn->close();
+
+echo($outp);
+?>
